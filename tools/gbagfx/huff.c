12d6a816fc4c9edf41aa541f029784b847788a8f
PikalaxALT <pikalaxalt@gmail.com>
2019-01-27 23:05:01 -0500

    Partially working huffcomp

--- tools/gbagfx/huff.c
+++ tools/gbagfx/huff.c
@@ -9,6 +9,64 @@ static int cmp_tree(const void * a0, const void * b0) {
     return ((struct HuffData *)a0)->value - ((struct HuffData *)b0)->value;
 }
 
+int msort(void * data, size_t count, size_t size, int (*cmp)(const void *, const void *)) {
+    void * buffer;
+    void * leftPtr;
+    void * rightPtr;
+    int i;
+
+    switch (count) {
+        case 0:
+        case 1:
+            return 1;
+        case 2:
+            if (cmp(data, data + size) > 0) {
+                buffer = malloc(size);
+                if (buffer == NULL)
+                    return 0;
+                memcpy(buffer, data, size);
+                memcpy(data, data + size, size);
+                memcpy(data + size, buffer, size);
+                free(buffer);
+            }
+            break;
+        default:
+            buffer = calloc(count, size);
+            if (buffer == NULL)
+                return 0;
+            memcpy(buffer, data, count * size);
+            leftPtr = buffer;
+            rightPtr = buffer + count / 2 * size;
+            if (!msort(leftPtr, count / 2, size, cmp))
+                return 0;
+            if (!msort(rightPtr, count / 2 + (count & 1), size, cmp))
+                return 0;
+            for (i = 0; i < count; i++) {
+                if (cmp(leftPtr, rightPtr) <= 0) {
+                    memcpy(data + i * size, leftPtr, size);
+                    leftPtr += size;
+                    if (leftPtr == buffer + count / 2 * size) break;
+                } else {
+                    memcpy(data + i * size, rightPtr, size);
+                    rightPtr += size;
+                    if (rightPtr == buffer + count * size) break;
+                }
+            }
+            if (++i < count) {
+                if (leftPtr < buffer + count / 2 * size) {
+                    memcpy(data + i * size, leftPtr, buffer + count / 2 * size - leftPtr);
+                }
+                else {
+                    memcpy(data + i * size, rightPtr, buffer + count * size - rightPtr);
+                }
+            }
+            free(buffer);
+            break;
+    }
+
+    return 1;
+}
+
 static inline void dump_bits(unsigned char * dest, int * destPos, uint32_t * buff) {
     dest[*destPos] = *buff;
     dest[*destPos + 1] = *buff >> 8;
@@ -18,44 +76,70 @@ static inline void dump_bits(unsigned char * dest, int * destPos, uint32_t * buf
     *destPos += 4;
 }
 
-static int write_tree(unsigned char * dest, struct HuffBranch * tree, struct BitEncoding * encoding, int offset, int skip, uint32_t curBitstream, int depth) {
-    int next = (offset & ~1) + skip + 2;
-    int outSize = 2;
-    depth++;
+void create_bit_encoding(struct HuffBranch * tree, struct BitEncoding * encoding, uint32_t curBitstream, int depth) {
 
+    HuffNode_t * left = tree->left;
+    HuffNode_t * right = tree->right;
+
+    depth++;
     uint32_t curLeftEncoding = curBitstream << 1;
     uint32_t curRightEncoding = curLeftEncoding | 1;
 
-    dest[offset] = (skip / 2) & 0x3F;
-
     for (int i = 0; i < depth - 1; i++) {
         fputc('\t', stderr);
     }
-    if (tree->right->header.isLeaf) {
-        fprintf(stderr, "RIGHT LEAF %d\n", tree->right->leaf.key);
-        dest[offset] |= 0x40;
-        dest[next + 1] = tree->right->leaf.key;
-        encoding[tree->right->leaf.key].nbits = depth;
-        encoding[tree->right->leaf.key].bitstring = curRightEncoding;
-    } else {
-        fprintf(stderr, "RIGHT BRANCH\n");
-        outSize += write_tree(dest, &tree->right->branch, encoding, next + 1, 0, curRightEncoding, depth);
-    }
-    for (int i = 0; i < depth - 1; i++) {
-        fputc('\t', stderr);
-    }
-    if (tree->left->header.isLeaf) {
-        fprintf(stderr, "LEFT LEAF %d\n", tree->left->leaf.key);
-        dest[offset] |= 0x80;
-        dest[next] = tree->left->leaf.key;
-        encoding[tree->left->leaf.key].nbits = depth;
-        encoding[tree->left->leaf.key].bitstring = curLeftEncoding;
+    if (left->header.isLeaf) {
+        fprintf(stderr, "LEFT LEAF %d\n", left->leaf.key);
+        encoding[left->leaf.key].nbits = depth;
+        encoding[left->leaf.key].bitstring = curLeftEncoding;
     } else {
         fprintf(stderr, "LEFT BRANCH\n");
-        outSize += write_tree(dest, &tree->left->branch, encoding, next, outSize, curLeftEncoding, depth);
+        create_bit_encoding(&left->branch, encoding, curLeftEncoding, depth);
     }
 
-    return outSize;
+    for (int i = 0; i < depth - 1; i++) {
+        fputc('\t', stderr);
+    }
+    if (right->header.isLeaf) {
+        fprintf(stderr, "RIGHT LEAF %d\n", right->leaf.key);
+        encoding[right->leaf.key].nbits = depth;
+        encoding[right->leaf.key].bitstring = curRightEncoding;
+    } else {
+        fprintf(stderr, "RIGHT BRANCH\n");
+        create_bit_encoding(&right->branch, encoding, curRightEncoding, depth);
+    }
+}
+
+static void lateral_traversal() {
+
+}
+
+static int write_tree(unsigned char * dest, HuffNode_t * tree, int offset) {
+    static int skip = 0;
+
+    int next;
+
+    HuffNode_t * left = tree->branch.left;
+    HuffNode_t * right = tree->branch.right;
+
+    dest[offset] = (skip - (offset - 4) / 2) & 0x3F;
+    skip++;
+    next = 4 + skip * 2;
+
+    if (left->header.isLeaf) {
+        dest[offset] |= 0x80;
+        dest[next] = left->leaf.key;
+    } else {
+        write_tree(dest, left, next);
+    }
+
+    if (right->header.isLeaf) {
+        dest[offset] |= 0x40;
+        dest[next + 1] = right->leaf.key;
+    } else {
+        write_tree(dest, right, next + 1);
+    }
+    return skip;
 }
 
 static void write_bits(unsigned char * dest, int * destPos, struct BitEncoding * encoding, int value, uint32_t * buff, int * buffBits) {
@@ -64,16 +148,16 @@ static void write_bits(unsigned char * dest, int * destPos, struct BitEncoding *
 
     if (*buffBits + nbits >= 32) {
         int diff = *buffBits + nbits - 32;
-        *buff >>= nbits - diff;
-        *buff |= bitstring << (32 - nbits + diff);
-        bitstring >>= nbits - diff;
+        *buff <<= nbits - diff;
+        *buff |= bitstring >> diff;
+        bitstring &= ~(1 << diff);
         nbits = diff;
         dump_bits(dest, destPos, buff);
         *buffBits = 0;
     }
     if (nbits != 0) {
-        *buff >>= nbits;
-        *buff |= bitstring << (32 - nbits);
+        *buff <<= nbits;
+        *buff |= bitstring;
         *buffBits += nbits;
     }
 }
@@ -119,13 +203,10 @@ unsigned char * HuffCompress(unsigned char * src, int srcSize, int * compressedS
     HuffNode_t * endptr = freqs + nitems - 2;
 
     for (int i = 0; i < nitems - 1; i++) {
-        qsort(freqs, nitems - i, sizeof(HuffNode_t), cmp_tree);
+        if (!msort(freqs, nitems - i, sizeof(HuffNode_t), cmp_tree))
+            goto fail;
         HuffNode_t * left = freqs;
         HuffNode_t * right = freqs + 1;
-        if (left->header.isLeaf && !right->header.isLeaf) {
-            left++;
-            right--;
-        }
         tree[i * 2] = *right;
         tree[i * 2 + 1] = *left;
         for (int j = 0; j < nitems - i - 2; j++)
@@ -137,7 +218,10 @@ unsigned char * HuffCompress(unsigned char * src, int srcSize, int * compressedS
         endptr--;
     }
 
-    int treeSize = write_tree(dest, &freqs->branch, encoding, 5, 0, 0, 0);
+    int treeSize = 0;
+    create_bit_encoding(&freqs->branch, encoding, 0, 0);
+    treeSize = write_tree(dest, freqs, 5);
+    treeSize *= 2;
     free(tree);
     free(freqs);
 
@@ -173,6 +257,36 @@ fail:
     FATAL_ERROR("Fatal error while decompressing Huff file.\n");
 }
 
+static void read_huff_tree(unsigned char * tree, int offset, int depth) {
+    static int skip = 1;
+    depth++;
+    int next = (offset & ~1) + ((tree[offset] & 0x3F) + 1) * 2;
+    fprintf(stderr, "%d --> %d (%d)\n", offset, next, skip);
+    skip++;
+
+    for (int i = 0; i < depth - 1; i++) {
+        fputc('\t', stderr);
+    }
+
+    if (tree[offset] & 0x80) {
+        fprintf(stderr, "LEFT LEAF %d\n", tree[next]);
+    } else {
+        fprintf(stderr, "LEFT BRANCH\n");
+        read_huff_tree(tree, next, depth);
+    }
+
+    for (int i = 0; i < depth - 1; i++) {
+        fputc('\t', stderr);
+    }
+
+    if (tree[offset] & 0x40) {
+        fprintf(stderr, "RIGHT LEAF %d\n", tree[next + 1]);
+    } else {
+        fprintf(stderr, "RIGHT BRANCH\n");
+        read_huff_tree(tree, next + 1, depth);
+    }
+}
+
 unsigned char * HuffDecompress(unsigned char * src, int srcSize, int * uncompressedSize_p) {
     if (srcSize < 4)
         goto fail;
@@ -189,6 +303,7 @@ unsigned char * HuffDecompress(unsigned char * src, int srcSize, int * uncompres
         goto fail;
 
     int treePos = 5;
+    read_huff_tree(src, 5, 0);
     int treeSize = (src[4] + 1) * 2;
     int srcPos = 4 + treeSize;
     int destPos = 0;
