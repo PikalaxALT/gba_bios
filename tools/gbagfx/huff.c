db05784f6959b6b8f70e507f3028de6aa35b890d
PikalaxALT <pikalaxalt@gmail.com>
2019-02-04 08:33:14 -0500

    More efficient mergesort

--- tools/gbagfx/huff.c
+++ tools/gbagfx/huff.c
@@ -9,81 +9,87 @@ static int cmp_tree(const void * a0, const void * b0) {
     return ((struct HuffData *)a0)->value - ((struct HuffData *)b0)->value;
 }
 
-int msort(void * data, size_t count, size_t size, int (*cmp)(const void *, const void *)) {
+typedef int (*cmpfun)(const void *, const void *);
+
+int msort_r(void * data, size_t count, size_t size, cmpfun cmp, void * buffer) {
     /*
      * Out-of-place mergesort (stable sort)
      * Returns 1 on success, 0 on failure
      */
-    void * buffer;
     void * leftPtr;
     void * rightPtr;
+    void * leftEnd;
+    void * rightEnd;
     int i;
 
     switch (count) {
     case 0:
+        // Should never be here
+        return 0;
+
     case 1:
         // Nothing to do here
-        return 1;
+        break;
 
     case 2:
         // Swap the two entries if the right one compares higher.
         if (cmp(data, data + size) > 0) {
-            buffer = malloc(size);
-            if (buffer == NULL)
-                return 0;
             memcpy(buffer, data, size);
             memcpy(data, data + size, size);
             memcpy(data + size, buffer, size);
-            free(buffer);
         }
         break;
     default:
         // Merge sort out-of-place.
-        // Because this function is recursive, total allocation ends up
-        // being just under twice the original data size.
-        buffer = calloc(count, size);
-        if (buffer == NULL)
-            return 0;
-        memcpy(buffer, data, count * size);
-        leftPtr = buffer;
-        rightPtr = buffer + count / 2 * size;
+        leftPtr = data;
+        leftEnd = rightPtr = data + count / 2 * size;
+        rightEnd = data + count * size;
 
         // Sort the left half
-        if (!msort(leftPtr, count / 2, size, cmp))
+        if (!msort_r(leftPtr, count / 2, size, cmp, buffer))
             return 0;
 
         // Sort the right half
-        if (!msort(rightPtr, count / 2 + (count & 1), size, cmp))
+        if (!msort_r(rightPtr, count / 2 + (count & 1), size, cmp, buffer))
             return 0;
 
-        // Merge the sorted halves
-        for (i = 0; i < count; i++) {
+        // Merge the sorted halves out of place
+        i = 0;
+        do {
             if (cmp(leftPtr, rightPtr) <= 0) {
-                memcpy(data + i * size, leftPtr, size);
+                memcpy(buffer + i * size, leftPtr, size);
                 leftPtr += size;
-                if (leftPtr == buffer + count / 2 * size) break;
             } else {
-                memcpy(data + i * size, rightPtr, size);
+                memcpy(buffer + i * size, rightPtr, size);
                 rightPtr += size;
-                if (rightPtr == buffer + count * size) break;
             }
-        }
+
+        } while (++i < count && leftPtr < leftEnd && rightPtr < rightEnd);
 
         // Copy the remainder
-        if (++i < count) {
-            if (leftPtr < buffer + count / 2 * size) {
-                memcpy(data + i * size, leftPtr, buffer + count / 2 * size - leftPtr);
+        if (i < count) {
+            if (leftPtr < leftEnd) {
+                memcpy(buffer + i * size, leftPtr, leftEnd - leftPtr);
             }
             else {
-                memcpy(data + i * size, rightPtr, buffer + count * size - rightPtr);
+                memcpy(buffer + i * size, rightPtr, rightEnd - rightPtr);
             }
         }
 
-        free(buffer);
+        // Copy the merged data back
+        memcpy(data, buffer, count * size);
         break;
     }
 
     return 1;
+}
+
+int msort(void * data, size_t count, size_t size, cmpfun cmp) {
+    void * buffer = malloc(count * size);
+    if (buffer == NULL) return 0;
+    int result = msort_r(data, count, size, cmp, buffer);
+    free(buffer);
+    return result;
 }
 
 static void write_tree(unsigned char * dest, HuffNode_t * tree, int nitems, struct BitEncoding * encoding) {
